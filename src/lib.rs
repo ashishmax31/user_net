74cdf0f39582b17988fad5d0f7cbb02cce2147d6
Ashish Anand <ashishmax31@gmail.com>
2020-08-24 00:58:06 +0530

    add library functions to read from link layer sockets

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,68 @@
-// use libc::size_t;
-use libc::socket;
-
+use libc::{c_int, c_void, setsockopt, size_t, socket, socklen_t};
 use nix::errno;
+use socket::htons;
+use std::ffi::CString;
 
-pub fn open_link_layer_socket() -> Result<i32, &'static str> {
+pub fn create_link_layer_socket() -> Result<c_int, &'static str> {
+    let fd;
+    unsafe {
+        fd = socket(
+            libc::AF_PACKET as c_int,
+            libc::SOCK_RAW as c_int,
+            htons(libc::ETH_P_ALL as u16) as c_int,
+        );
+    }
+    if fd < 0 {
+        let err = errno::Errno::last();
+        Err(err.desc())
+    } else {
+        Ok(fd)
+    }
+}
+
+pub fn bind_to_device(fd: c_int, interface: &'static str) -> Result<(), &'static str> {
+    let device_name = match CString::new(interface) {
+        Ok(c_string) => c_string,
+        Err(_) => return Err("Null byte found in the device name string!"),
+    };
+    let device_name_ptr = device_name.as_ptr() as *const c_void;
+
     let res;
     unsafe {
-        res = socket(libc::AF_PACKET, libc::SOCK_RAW, libc::ETH_P_ALL);
+        res = setsockopt(
+            fd,
+            libc::SOL_SOCKET as c_int,
+            libc::SO_BINDTODEVICE as c_int,
+            device_name_ptr,
+            interface.len() as socklen_t,
+        );
     }
-    match res {
-        err_no if err_no < 0 => {
-            let err = errno::Errno::last();
-            Err(err.desc())
+
+    if res == 0 {
+        Ok(())
+    } else {
+        let err = errno::Errno::last();
+        Err(err.desc())
+    }
+}
+
+pub fn read_from_socket(fd: c_int, mtu: usize) {
+    let mut buffer: Vec<u8> = vec![0; mtu];
+    let buffer_ptr = buffer.as_mut_ptr() as *mut c_void;
+
+    unsafe {
+        loop {
+            let res = libc::recv(fd, buffer_ptr, buffer.capacity() as size_t, 0 as c_int);
+            if res < 0 {
+                let err = errno::Errno::last();
+                eprintln!("{}", err.desc());
+                panic!(err.desc());
+            } else {
+                println!("bytes read: {}", res as i32);
+                // TODO:
+                // Parse the raw ethernet packets -> IPv4 packet -> layer 3 packet
+                //
+            }
         }
-        fd => Ok(fd),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use user_net::*;
 
 fn main() {
-    let socket = match open_link_layer_socket(){
+    let socket = match create_link_layer_socket() {
         Ok(fd) => fd,
-        Err(err) => panic!(err)
+        Err(err) => panic!(err),
     };
+    match bind_to_device(socket, "enp2s0") {
+        Ok(_) => println!("Successful binding with interface!"),
+        Err(err) => eprintln!("{}", err),
+    };
+    read_from_socket(socket, 1024);
 }
