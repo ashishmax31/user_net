a2d2b561f55d0dbf6bb18d86e92096f1f9db3a17
Ashish Anand <ashishmax31@gmail.com>
2020-08-26 01:17:31 +0530

    use a channel and thread to read from a socket

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+#[macro_use]
+extern crate ioctl_macros;
+
 use libc::{c_int, c_void, setsockopt, size_t, socket, socklen_t};
 use nix::errno;
-use socket::htons;
 use std::ffi::CString;
+use std::sync::mpsc::channel;
+use std::thread;
+mod tap;
+pub use tap::create_tap_device;
 
 pub fn create_link_layer_socket() -> Result<c_int, &'static str> {
     let fd;
@@ -9,7 +15,7 @@ pub fn create_link_layer_socket() -> Result<c_int, &'static str> {
         fd = socket(
             libc::AF_PACKET as c_int,
             libc::SOCK_RAW as c_int,
-            htons(libc::ETH_P_ALL as u16) as c_int,
+            (libc::ETH_P_ALL as u16).to_be() as c_int,
         );
     }
     if fd < 0 {
@@ -46,23 +52,28 @@ pub fn bind_to_device(fd: c_int, interface: &'static str) -> Result<(), &'static
     }
 }
 
-pub fn read_from_socket(fd: c_int, mtu: usize) {
+pub fn read_from_socket(fd: c_int, mtu: usize) -> std::sync::mpsc::Receiver<std::vec::Vec<u8>> {
+    let (tx, rx) = channel();
     let mut buffer: Vec<u8> = vec![0; mtu];
-    let buffer_ptr = buffer.as_mut_ptr() as *mut c_void;
 
-    unsafe {
+    thread::spawn(move || {
+        let buffer_ptr = buffer.as_mut_ptr() as *mut c_void;
         loop {
-            let res = libc::recv(fd, buffer_ptr, buffer.capacity() as size_t, 0 as c_int);
-            if res < 0 {
-                let err = errno::Errno::last();
-                eprintln!("{}", err.desc());
-                panic!(err.desc());
-            } else {
-                println!("bytes read: {}", res as i32);
-                // TODO:
-                // Parse the raw ethernet packets -> IPv4 packet -> layer 3 packet
-                //
+            unsafe {
+                let res = libc::read(fd, buffer_ptr, buffer.capacity() as size_t);
+                if res < 0 {
+                    let err = errno::Errno::last();
+                    eprintln!("{}", err.desc());
+                    panic!(err.desc());
+                } else {
+                    println!("bytes read: {}", res as i32);
+                    tx.send(buffer.clone()).unwrap();
+                    // TODO:
+                    // Parse the raw ethernet packets -> IPv4 packet -> layer 3 packet
+                    //
+                }
             }
         }
-    }
+    });
+    return rx;
 }
